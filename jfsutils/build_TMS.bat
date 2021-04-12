84fc3410e1dbb256669bc359709a774b6265e2ab
Christian Wünsch <c.wuensch@gmail.com>
2021-04-12 11:50:04 +0200

    Compiler-Pfade angepasst

--- jfsutils/build_TMS.bat
+++ jfsutils/build_TMS.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 rem if exist build rmdir /s /q build
 mkdir build
 cd build
-set PATH=%TFROOT%\gccForTMS\crosstool\bin;%TFROOT%\gccForTMS\crosstool\mipsel-linux-uclibc\bin;%TFROOT%\Cygwin_mini\bin;C:\Programme\Cygwin\bin
+set PATH=%TFROOT%\gccForTMS\crosstool\bin;%TFROOT%\gccForTMS\crosstool\mipsel-linux-uclibc\bin;%TFROOT%\Cygwin\bin;C:\Programme\Cygwin\bin
 set CFLAGS=-pipe -Os -static -W -Wall
 set LDFLAGS=-static
 rem bash ../configure --host=mipsel-linux-uclibc
